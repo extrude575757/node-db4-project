0f41a8c2fe6cb38098116a32152df412d12a89a0
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-02-04 21:47:44 -0800

    DBstart

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const db = require('../data/db-config.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get('/api/recipe', (req, res) => {
+  // get all species from the database
+  db('recipes')
+    .then(r => {
+      res.status(200).json(r);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
